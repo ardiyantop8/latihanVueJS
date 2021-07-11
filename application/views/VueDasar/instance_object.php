e2805aad89e72f90d9b75f4fa74bae951bca1d90
ardiputra04 <ardidesti04@gmail.com>
2021-07-11 20:26:31 +0700

    membuat variable data

--- application/views/VueDasar/instance_object.php
+++ application/views/VueDasar/instance_object.php
@@ -10,13 +10,21 @@
 
 <body>
     <div id="app-latihan-intance-object">
+        <p>Nama Saya : {{nama}}</p>
+        <p>Umur Saya : {{umur}}</p>
     </div>
 
     <script src="<?= base_url('assets/js/'); ?>vue.js"></script>
     <script>
+        var pribadi = {
+            nama: "Ardiyanto Putra",
+            umur: "27 Tahun",
+            kelamin: "Laki-laki"
+        }
         const vm = new Vue({
-            el: '#app-latihan-intance-object'
-        })
+            el: '#app-latihan-intance-object',
+            data: pribadi
+        });
     </script>
 </body>
 
